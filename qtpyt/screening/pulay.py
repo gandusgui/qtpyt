9835125c1d45a6baa7fbc43322fb1e94d4c55cad
ggandus <ggandus@student.ethz.ch>
2022-06-21 15:38:44 +0200

    add: pulay mix density matrix

--- qtpyt/screening/pulay.py
+++ qtpyt/screening/pulay.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import numpy as np
 from numba import njit, prange
 from qtpyt.base._kernels import dagger
@@ -86,7 +88,127 @@ class ArrayList:
         return self.array[i]
 
 
-class Pulay:
+# class Pulay:
+#     """Pulay mixing scheme.
+
+#          __
+#          \
+#     new =     (1-alpha) coeff[n] inp[n] + alpha coeff[n] out[n]
+#          /_ n
+
+#     Args:
+#         alpha : float
+#             mixing factor
+#         N : # of iterations to store
+#     """
+
+#     def __init__(self, alpha=0.4, N=3):
+#         self.alpha = alpha
+#         self.N = N
+#         self.Gr_inp = []  # ArrayList(N)
+#         self.Gr_out = []  # ArrayList(N)
+#         self.eps = []
+
+#     def _append(self, G_list, G):
+#         G_list.append(G)
+#         if len(G_list) > self.N:
+#             G_list.pop(0)
+
+#     def append_input(self, gf: DistGreenFunction):
+#         """Append input."""
+#         self._append(self.Gr_inp, gf.arrays["r"].copy())
+
+#     def append_output(self, gf: DistGreenFunction):
+#         """Append output."""
+#         self._append(self.Gr_out, gf.arrays["r"].copy())
+#         # Compute inner product with all previous (and current) residuals.
+#         N = len(self.Gr_out)
+#         i = N - 1
+#         eps = [None] * N
+#         for j in range(N):
+#             eps[j] = np.dot(
+#                 (self.Gr_out[i] - self.Gr_inp[i]).flat,
+#                 (self.Gr_out[j] - self.Gr_inp[j]).flat,
+#             ) + np.dot(
+#                 (self.Gr_out[i].real - self.Gr_inp[i].real).flat,
+#                 (self.Gr_out[j].real - self.Gr_inp[j].real).flat,
+#             )
+#         # Append lower(upper) triangular entries of Pulay matrix.
+#         self.eps.append(eps)
+#         if len(self.eps) > self.N:
+#             self.eps.pop(0)  # Discard row
+#             for eps in self.eps[:-1]:
+#                 eps.pop(0)  # Discard column
+
+#     def compute_new_coeff(self):
+#         """Compute Pulay's mixing coefficients."""
+#         N = len(self.Gr_out)
+#         A = np.zeros((N + 1, N + 1))
+#         B = np.zeros(N + 1)
+#         B[N] = 1.0
+#         # Fill lower triangular.
+#         for i, eps in enumerate(self.eps):
+#             A[i, : i + 1] = eps
+#         # Fill symmetric.
+#         tri2full(A)
+#         A = comm_sum(A)
+#         A[:N, N] = 1.0
+#         A[N, :N] = 1.0
+#         self.A = A
+#         self.coeff = np.linalg.solve(A, B)[:-1]
+#         return self.coeff
+
+#     def compute_new_input(self, G_inp: ArrayList, G_out: ArrayList, new=None):
+#         """Mix previous inputs and outputs and produce new input."""
+#         if new is None:
+#             new = np.zeros_like(G_inp[0])
+#         else:
+#             new[:] = 0.0
+#         coeff = self.compute_new_coeff()
+#         for i in range(len(coeff)):
+#             new += (1 - self.alpha) * coeff[i] * G_inp[i] + self.alpha * coeff[
+#                 i
+#             ] * G_out[i]
+#         # _mix(self.alpha, coeff, G_inp.array, G_out.array, new)
+#         return new
+
+#     def update_input(self, gf: DistGreenFunction):
+#         """Update Green's function arrays with input computed from Pulay."""
+#         Gr = gf.arrays["r"]
+#         Gl = gf.arrays["l"]
+#         self.compute_new_input(self.Gr_inp, self.Gr_out, Gr)
+#         fermi = gf.gf0.fermi
+#         for e, energy in enumerate(gf.energies):
+#             Gl[e] = -fermi(energy) * (Gr[e] - dagger(Gr[e]))
+#         self.append_input(gf)
+
+#     def initialize(self, gf: DistGreenFunction, **kwargs):
+#         """Update Green's function and initialize Pulay."""
+#         child = None
+#         if gf.parent is not None:
+#             child = gf
+#             gf = gf.parent
+#         gf.update(lg=False, **kwargs)
+#         self.append_input(gf)
+#         gf.convert_retarded()
+#         if child is not None:
+#             child.update_from_parent(gf)
+
+#     def step(self, gf: DistGreenFunction, **kwargs):
+#         """Update Green's function and Pulay's internals."""
+#         child = None
+#         if gf.parent is not None:
+#             child = gf
+#             gf = gf.parent
+#         gf.update(lg=False, **kwargs)
+#         self.append_output(gf)
+#         self.update_input(gf)  # << Gr, G<
+#         gf.convert_retarded()  # << G<, G>
+#         if child is not None:
+#             child.update_from_parent(gf)
+
+
+class PulayMixer:
     """Pulay mixing scheme.
 
          __
@@ -103,8 +225,8 @@ class Pulay:
     def __init__(self, alpha=0.4, N=3):
         self.alpha = alpha
         self.N = N
-        self.Gr_inp = []  # ArrayList(N)
-        self.Gr_out = []  # ArrayList(N)
+        self.inp = []  # ArrayList(N)
+        self.out = []  # ArrayList(N)
         self.eps = []
 
     def _append(self, G_list, G):
@@ -112,25 +234,19 @@ class Pulay:
         if len(G_list) > self.N:
             G_list.pop(0)
 
-    def append_input(self, gf: DistGreenFunction):
+    def append_input(self, D: np.ndarray):
         """Append input."""
-        self._append(self.Gr_inp, gf.arrays["r"].copy())
+        self._append(self.inp, D)
 
-    def append_output(self, gf: DistGreenFunction):
+    def append_output(self, D: np.ndarray):
         """Append output."""
-        self._append(self.Gr_out, gf.arrays["r"].copy())
+        self._append(self.out, D)
         # Compute inner product with all previous (and current) residuals.
-        N = len(self.Gr_out)
+        N = len(self.out)
         i = N - 1
         eps = [None] * N
         for j in range(N):
-            eps[j] = np.dot(
-                (self.Gr_out[i].imag - self.Gr_inp[i].imag).flat,
-                (self.Gr_out[j].imag - self.Gr_inp[j].imag).flat,
-            ) + np.dot(
-                (self.Gr_out[i].real - self.Gr_inp[i].real).flat,
-                (self.Gr_out[j].real - self.Gr_inp[j].real).flat,
-            )
+            eps[j] = np.vdot(self.out[i] - self.inp[i], self.out[j] - self.inp[j]).real
         # Append lower(upper) triangular entries of Pulay matrix.
         self.eps.append(eps)
         if len(self.eps) > self.N:
@@ -140,7 +256,7 @@ class Pulay:
 
     def compute_new_coeff(self):
         """Compute Pulay's mixing coefficients."""
-        N = len(self.Gr_out)
+        N = len(self.out)
         A = np.zeros((N + 1, N + 1))
         B = np.zeros(N + 1)
         B[N] = 1.0
@@ -149,59 +265,34 @@ class Pulay:
             A[i, : i + 1] = eps
         # Fill symmetric.
         tri2full(A)
-        A = comm_sum(A)
+        # A = comm_sum(A)
         A[:N, N] = 1.0
         A[N, :N] = 1.0
         self.A = A
         self.coeff = np.linalg.solve(A, B)[:-1]
         return self.coeff
 
-    def compute_new_input(self, G_inp: ArrayList, G_out: ArrayList, new=None):
+    def compute_new_input(self, new=None):
         """Mix previous inputs and outputs and produce new input."""
         if new is None:
-            new = np.zeros_like(G_inp[0])
+            new = np.zeros_like(self.inp[0])
         else:
             new[:] = 0.0
         coeff = self.compute_new_coeff()
         for i in range(len(coeff)):
-            new += (1 - self.alpha) * coeff[i] * G_inp[i] + self.alpha * coeff[
+            new += (1 - self.alpha) * coeff[i] * self.inp[i] + self.alpha * coeff[
                 i
-            ] * G_out[i]
-        # _mix(self.alpha, coeff, G_inp.array, G_out.array, new)
+            ] * self.out[i]
         return new
 
-    def update_input(self, gf: DistGreenFunction):
-        """Update Green's function arrays with input computed from Pulay."""
-        Gr = gf.arrays["r"]
-        Gl = gf.arrays["l"]
-        self.compute_new_input(self.Gr_inp, self.Gr_out, Gr)
-        fermi = gf.gf0.fermi
-        for e, energy in enumerate(gf.energies):
-            Gl[e] = -fermi(energy) * (Gr[e] - dagger(Gr[e]))
-        self.append_input(gf)
-
-    def initialize(self, gf: DistGreenFunction, **kwargs):
-        """Update Green's function and initialize Pulay."""
-        child = None
-        if gf.parent is not None:
-            child = gf
-            gf = gf.parent
-        gf.update(lg=False, **kwargs)
-        self.append_input(gf)
-        gf.convert_retarded()
-        if child is not None:
-            child.update_from_parent(gf)
-
-    def step(self, gf: DistGreenFunction, **kwargs):
-        """Update Green's function and Pulay's internals."""
-        child = None
-        if gf.parent is not None:
-            child = gf
-            gf = gf.parent
-        gf.update(lg=False, **kwargs)
-        self.append_output(gf)
-        self.update_input(gf)  # << Gr, G<
-        gf.convert_retarded()  # << G<, G>
-        if child is not None:
-            child.update_from_parent(gf)
-
+    def __call__(self, func, D_inp, tol=1e-5, max_iter=100) -> Any:
+        self.append_input(D_inp)
+        iter = 0
+        eps = np.inf
+        while eps > tol and iter < max_iter:
+            iter += 1
+            D_out = func(D_inp)
+            self.append_output(D_out)
+            eps = abs((D_out - D_inp).real.sum())
+            D_inp = self.compute_new_input()
+            self.append_input(D_inp)
