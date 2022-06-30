2261fa78199caab1b2fea97e79fa99bc3cbbdc42
ggandus <ggandus@student.ethz.ch>
2022-06-30 17:24:50 +0200

    dev: elph couplings

---
+++ qtpyt/elph/selfph.py
@@ -0,0 +1,97 @@
+import numpy as np
+from qtpyt.elph.hilbert import hilbert
+from qtpyt.screening.distgf import DistGreenFunction
+from qtpyt.screening.langreth import LangrethPair
+
+kB = 8.6e-5  # Boltzmann
+nB = lambda w, kt: 1.0 / (np.exp(w / kt) - 1.0)
+
+
+def translate(a, s, out, w=1.0):
+    """Translate an array by s."""
+    if s > 0:
+        # f^-(x) = f(x-y)
+        out[s:] += w * a[:-s]
+    elif s < 0:
+        # f^+(x) = f(x+y)
+        s = abs(s)
+        out[:-s] += w * a[s:]
+    else:
+        out += w * s
+
+
+def setzeros(*arrays):
+    """Set values of arrays to zero."""
+    for a in arrays:
+        a[:] = 0.0
+
+
+class ElphFockSelfEnergy(LangrethPair):
+    """Electron-phonon Fock self-energy.
+    
+    
+    Args:
+        gf : DistGreenFunction
+            electron Green's function.
+        g_lii : np.ndarray, shape = (# modes, # basis, # basis)
+            electron-phonon coupling matrix.
+        w_l : np.ndarray, shape = (# modes)
+            phonon frequencies.
+    """
+
+    def __init__(
+        self, gf: DistGreenFunction, g_lii: np.ndarray, w_l: np.ndarray
+    ) -> None:
+        self.gf = gf
+        self.g_lii = g_lii
+        self.w_l = w_l
+        self.nph_l = nB(w_l, self.gf.gf0.kt)
+        super().__init__(gf.global_energies, gf.no, dtype=complex)
+        # We need retarded and lesser
+        self.arrays["r"] = self.arrays.pop("g")
+
+    def update_elph(self):
+
+        Gl = self.collect_energies(self.gf.arrays["l"])
+        Gg = self.collect_energies(self.gf.arrays["g"])
+
+        Gl_minus = np.empty_like(Gl)
+        Gl_plus = np.empty_like(Gl)
+        Gg_minus = np.empty_like(Gl)
+        Gg_plus = np.empty_like(Gl)
+
+        Sr = self.arrays["r"]
+        Sl = self.arrays["l"]
+        setzeros(Sr, Sl)
+
+        for w, g_ii, nph in zip(self.w_l, self.g_lii, self.nph_l):
+
+            s = w / self.de
+            r = s % 1  # number from (0,1)
+            s = int(s - r)
+
+            setzeros(Gl_minus, Gl_plus, Gg_minus, Gg_plus)
+
+            # interpolate
+            for w in (1 - r, r):
+                w = round(w, 5)
+                if w > 0.0:
+                    translate(Gl, s, Gl_minus, w)
+                    translate(Gg, s, Gg_minus, w)
+                    translate(Gl, -s, Gl_plus, w)
+                    translate(Gg, -s, Gg_plus, w)
+                s += 1
+
+            l = self.collect_orbitals(nph * Gl_minus + (1 + nph) * Gl_plus)
+            g = self.collect_orbitals(nph * Gg_minus + (1 + nph) * Gg_plus)
+
+            l = np.einsum("ij,ejk,kl->eil", g_ii, l, g_ii, optimize=True)
+            g = np.einsum("ij,ejk,kl->eil", g_ii, l, g_ii, optimize=True)
+
+            Sd = 0.5 * (g - l)
+
+            Sl += l
+            Sr += Sd - 1.0j * hilbert(Sd)
+
+    def retarded(self, energy):
+        return self.arrays["r"][np.searchsorted(self.energies, energy)]
